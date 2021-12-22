48cfbd51640095b28b4c12174f02ce4c1e23866d
Liam Brewster <liam.brewster@hotmail.co.uk>
2021-12-22 21:53:43 +0000

    added the create url function

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
+mongoose.connect('mongodb://localhost:27017/urlShortener', {
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+})
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
 
-app.get('/', (req,res) => {
-    res.render('index')
+app.get('/', async (req,res) => {
+    const shortUrls = await ShortUrl.find()
+    res.render('index', {shortUrls})
 
+})
+
+app.post('/shortUrls', async (req,res) => {
+await ShortUrl.create({full:req.body.fullURL})
+res.redirect('/')
 })
 
 
